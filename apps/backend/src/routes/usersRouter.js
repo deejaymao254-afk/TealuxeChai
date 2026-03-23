66c07afcd708dc509674b5485b81a48ed24b1a7b
Michael Odada <mikeodada333@gmail.com>
2026-03-24 02:26:27 +0300

    fix: correct db import (named export)

--- apps/backend/src/routes/usersRouter.js
+++ apps/backend/src/routes/usersRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import pool from "../config/db.js";
+import { pool } from "../config/db.js";
 import { generateToken } from "../controllers/auth.js";
 import { createUser, getUserByPhone } from "../services/userService.js";
 
