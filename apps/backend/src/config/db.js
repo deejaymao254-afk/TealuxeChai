452e65e2165b4352b46849832f414ca8e91656cf
Michael Odada <mikeodada333@gmail.com>
2026-03-30 15:25:02 +0300

    LK

--- apps/backend/src/config/db.js
+++ apps/backend/src/config/db.js
@@ -4,22 +4,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
-
 const { Pool } = pg;
 
-// Create a single Pool instance
-const db = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
-// Test connection
-db.connect()
+pool.connect()
   .then(() => console.log("✅ Connected to PostgreSQL"))
   .catch(err => console.error("❌ DB connection error:", err.message));
-
-console.log("DB URL:", process.env.DATABASE_URL);
-
-// Export the Pool as default
-export default db;
