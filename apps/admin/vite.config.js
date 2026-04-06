57442b012ba38d170912bcf2ebc661a0423cbeab
Michael Odada <mikeodada333@gmail.com>
2026-04-06 15:37:40 +0300

    d

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["jwt-decode"], // externalize if needed
+    },
+  },
+
+  
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
         changeOrigin: true,
         secure: false,
-
       },
     },
   },
-})
-
+});
