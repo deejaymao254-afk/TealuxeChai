1aae3a8a34105b3083c5e552b708db3dc1e75628
Michael Odada <mikeodada333@gmail.com>
2026-04-06 16:37:10 +0300

    gfgf

--- apps/admin/src/main.jsx
+++ apps/admin/src/main.jsx
@@ -1,28 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import ErrorBoundary from "./middleware/ErrorBoundary";
 
-
-
-// GLOBAL STYLES
-import "./index.css";
-import "./App.css";
-
-
-
-export default function SafeLayout({ children }) {
-  return <div className="app-layout">{children}</div>;
-}
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
